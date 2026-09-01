868306f34d620c360001963c0185ff0bd4746811
Marco Carlino <marco.carlino@pagopa.it>
2026-09-01 12:01:07 +0200

    apply suggestion

--- variables.tf
+++ variables.tf
@@ -75,7 +75,7 @@ variable "saml_provider_arn" {
 }
 
 variable "self_service_saml_provider_arn" {
-  description = "The ARN of the IAM SAML identity provider for the Client VPN self-service portal. Defaults to saml_provider_arn when not set."
+  description = "The ARN of the IAM SAML identity provider for the Client VPN self-service portal. Defaults to saml_provider_arn when the portal is enabled and this value is not set."
   type        = string
   default     = null
 }
